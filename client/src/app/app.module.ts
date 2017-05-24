55e21f88d75562ee927ccf03c9dc88f6788efb1c
Paula Jimenez <pjimvel@gmail.com>
2017-05-24 17:21:27 +0200

    First Event routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,37 @@
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterializeModule } from 'angular2-materialize';
 
+import { EventService } from './services/event.service';
+
+import { RouterModule } from "@angular/router";
+import { routes } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { SingleEventComponent } from './single-event/single-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    SingleEventComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    EventService,
+    { provide: 'BASE_ENDPOINT', useValue: environment.baseEndpoint },
+    { provide: 'API_ENDPOINT', useValue: environment.apiEndpoint }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
